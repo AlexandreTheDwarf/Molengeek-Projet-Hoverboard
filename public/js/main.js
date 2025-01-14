eb1d2a825e9eaeafebfc22eb79c18cfbbbc9bccc
AlexandreTheDwarf <alexandre-vandewiele@outlook.com>
2025-01-14 11:19:26 +0100

    add a sound sorry

--- public/js/main.js
+++ public/js/main.js
@@ -6,15 +6,26 @@ function RandomColor() {
     return color[Math.floor(Math.random() * color.length)];
 }
 
-
 square.forEach(element => {
     element.addEventListener("mouseover", () => {
         const colorClass = RandomColor();
         element.classList.add(colorClass);
-        
+        playSound(randomSound());
         // Enlever la classe après 1 seconde (1000 ms)
         setTimeout(() => {
             element.classList.remove(colorClass);
         }, 500);
     });
 });
+
+let sound = ["buble1.wav", "buble2.wav", "buble3.wav", "buble4.wav", "buble5.wav"]
+
+function playSound(soundFile) {
+    audioSource.src = "./public/sound/" + soundFile; // Change la source du son
+    audioPlayer.load();           // Recharge l'élément audio avec la nouvelle source
+    audioPlayer.play();           // Joue le son
+}
+
+function randomSound (){
+    return sound[Math.floor(Math.random() * sound.length)]
+}
